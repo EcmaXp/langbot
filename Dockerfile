c6ccdd2c2a3817c9dcb3976fa769940c8726a236
ecmaxp <ecmaxp@ecmaxp.kr>
2024-04-08 02:16:28 +0900

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.12 as requirements-stage
+WORKDIR /tmp
+RUN apt update && apt install rustc -y
+RUN pip install poetry
+COPY ./pyproject.toml ./poetry.lock* /tmp/
+RUN poetry export -f requirements.txt --output requirements.txt --without-hashes
+
+FROM python:3.12-slim
+WORKDIR /code
+COPY --from=requirements-stage /tmp/requirements.txt /code/requirements.txt
+RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
+COPY ./main.py /code/
+CMD ["python", "-m", "main"]
---
+++ main.py
@@ -0,0 +1,406 @@
+from __future__ import annotations
+
+import asyncio
+import logging
+import os
+import tempfile
+from collections import defaultdict
+from copy import deepcopy
+from datetime import datetime
+from functools import cache
+from typing import List, Optional, cast
+
+import hikari
+import tiktoken
+from async_lru import alru_cache
+from langchain_community.callbacks.manager import get_openai_callback
+from langchain_community.callbacks.openai_info import OpenAICallbackHandler
+from langchain_core.language_models import BaseChatModel
+from langchain_core.messages import AIMessage, BaseMessage, HumanMessage, SystemMessage
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import (
+    ChatPromptTemplate,
+    MessagesPlaceholder,
+    HumanMessagePromptTemplate,
+)
+from langchain_core.runnables import Runnable, RunnablePassthrough
+from langchain_openai import ChatOpenAI
+
+__author__ = "EcmaXp <ecmaxp@ecmaxp.kr>"
+__version__ = "0.1"
+
+
+if os.name != "nt":
+    import uvloop
+
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+
+
+def get_chat_model(chat_model_name: str) -> BaseChatModel:
+    provider, chat_model_name = chat_model_name.split(":")
+    if provider == "openai":
+        return ChatOpenAI(model=chat_model_name)
+    else:
+        raise ValueError("Unknown provider")
+
+
+def get_tokens(chat_model: BaseChatModel, text: str) -> int:
+    if isinstance(chat_model, ChatOpenAI):
+        return _get_num_tokens_openai(chat_model.model_name, text)
+    else:
+        return chat_model.get_num_tokens(text)
+
+
+@cache
+def _get_num_tokens_openai(model_name: str, text: str) -> int:
+    encoding = tiktoken.encoding_for_model(model_name)
+    return len(encoding.encode(text))
+
+
+class Chat:
+    def __init__(
+        self,
+        history: list[BaseMessage],
+        chat_model: BaseChatModel,
+    ):
+        self.history = history
+        self.last_cb: OpenAICallbackHandler | None = None
+        self.chat_model = chat_model
+
+    def build_chat_chain(self) -> Runnable:
+        return (
+            RunnablePassthrough()
+            | ChatPromptTemplate.from_messages(
+                [MessagesPlaceholder(variable_name="chat_history")]
+            )
+            | self.chat_model
+            | StrOutputParser()
+        )
+
+    async def ask(
+        self,
+        text: Optional[str] = None,
+        *,
+        max_tokens: int,
+    ):
+        if text is not None:
+            self.history.append(HumanMessage(text))
+
+        available_tokens = max_tokens - self.get_tokens()
+        if available_tokens <= 0:
+            raise ValueError("No available tokens, please start a new chat.")
+
+        chat_chain = self.build_chat_chain()
+        output = await chat_chain.ainvoke({"chat_history": self.history})
+
+        self.history.append(AIMessage(output))
+        return output
+
+    def get_tokens(self) -> int:
+        return 3 + sum(
+            get_tokens(self.chat_model, item.content) + 5 for item in self.history
+        )
+
+    def copy(self):
+        return Chat(deepcopy(self.history), self.chat_model)
+
+    def print(self, message: hikari.Message | None = None):
+        print()
+        print("#", message.make_link(message.guild_id))
+        print(f"{message.author} @ {datetime.now().replace(microsecond=0)}:")
+        for no, item in enumerate(self.history):
+            print(f"{item.type}: {item.content}")
+
+        if self.last_cb is not None:
+            usage = self.last_cb
+            print(f"{usage.completion_tokens = }")
+            print(f"{usage.prompt_tokens = }")
+            print(f"{usage.total_tokens = }")
+            print(f"{usage.total_cost = }")
+
+    async def compress_large_messages(
+        self,
+        chat_compress_threshold: int,
+        message_compress_threshold: int,
+    ):
+        if self.get_tokens() < chat_compress_threshold:
+            return
+
+        for pos, item in enumerate(self.history[3:-3]):
+            if item["role"] == "system":
+                continue
+            elif item["tokens"] > message_compress_threshold:
+                item["content"] = await get_summary(item["content"])
+                item["tokens"] = get_tokens(self.chat_model, item["content"])
+
+
+@alru_cache(maxsize=1024, typed=True)
+async def get_summary(text: str) -> str:
+    chat_model = ChatOpenAI(model="gpt-3.5-turbo-0125")
+    chain = (
+        RunnablePassthrough()
+        | ChatPromptTemplate.from_messages(
+            [
+                SystemMessage("[system] Summarize the following:"),
+                HumanMessagePromptTemplate.from_template("{input}"),
+            ]
+        )
+        | chat_model
+        | StrOutputParser()
+    )
+
+    summary = await chain.ainvoke({"input": text})
+
+    old_num_tokens = get_tokens(chat_model, text)
+    new_num_tokens = get_tokens(chat_model, summary)
+
+    print(f"Summarized: {old_num_tokens} -> {new_num_tokens} tokens")
+    return summary
+
+
+class ChatGPT:
+    def __init__(self, bot: hikari.GatewayBot, config: dict):
+        self.bot = bot
+        self.config = config
+        self.chat_model = get_chat_model(config["chat_model"])
+        self.cached_channels = {}
+        self.cached_messages = defaultdict(dict)
+        self.cached_reply_ids: dict[int, set] = defaultdict(set)
+        config.setdefault("total_tokens", 0)
+        config.setdefault("total_cost", 0)
+
+    @property
+    def bot_id(self):
+        return self.bot.get_me().id
+
+    async def fetch_channel(self, channel_id: hikari.Snowflakeish):
+        if channel_id not in self.cached_channels:
+            self.cached_channels[channel_id] = await self.bot.rest.fetch_channel(
+                channel_id
+            )
+
+        return self.cached_channels[channel_id]
+
+    async def on_ready(self, event: hikari.ShardReadyEvent):
+        await self.update_presence()
+
+    async def on_message(
+        self, event: hikari.MessageCreateEvent | hikari.MessageUpdateEvent
+    ):
+        if not event.is_human:
+            return
+
+        if self.bot_id not in event.message.user_mentions_ids and (
+            event.message.referenced_message is None
+            or event.message.referenced_message.author.id != self.bot_id
+        ):
+            return
+
+        channel = await self.fetch_channel(event.channel_id)
+        if not isinstance(channel, hikari.TextableChannel):
+            return
+
+        await self.chatgpt(event.message)
+
+    async def on_message_edit(self, event: hikari.MessageUpdateEvent):
+        channel = await self.fetch_channel(event.channel_id)
+        if not isinstance(channel, hikari.TextableChannel):
+            return
+
+        if event.message.id in self.cached_reply_ids:
+            reply_ids = self.cached_reply_ids.pop(event.message.id, None)
+            if reply_ids:
+                await channel.delete_messages(reply_ids)
+
+        await self.on_message(event)
+
+    async def on_message_delete(self, event: hikari.MessageDeleteEvent):
+        channel = await self.fetch_channel(event.channel_id)
+        if not isinstance(channel, hikari.TextableChannel):
+            return
+
+        if event.message_id in self.cached_reply_ids:
+            reply_ids = self.cached_reply_ids.pop(event.message_id, None)
+            if reply_ids:
+                await channel.delete_messages(reply_ids)
+
+    async def chatgpt(self, message: hikari.Message):
+        channel = await self.fetch_channel(message.channel_id)
+
+        try:
+            chat = await self.build_chat(message)
+            if isinstance(chat.history[-1], SystemMessage) == "system":
+                await self.reply(message, "[SYSTEM] System message is set.")
+                return
+
+            with get_openai_callback() as cb:
+                async with channel.trigger_typing():
+                    await self.preprocessing_chat(message, chat)
+                    answer = await chat.ask(max_tokens=8192)
+                    await self.reply(message, answer)
+                    chat.print(message)
+
+                self.config["total_tokens"] += cb.total_tokens
+                self.config["total_cost"] += cb.total_cost
+        except Exception as e:
+            logging.exception("Error in chatgpt")
+            await self.reply(message, f":warning: **{type(e).__name__}**: {e}")
+
+        await self.update_presence()
+
+    async def preprocessing_chat(self, message: hikari.Message, chat: Chat):
+        title = f"{message.author} @ {datetime.now().replace(microsecond=0)}"
+
+        before_tokens = chat.get_tokens()
+        print(f"{title}: Requesting {before_tokens} tokens")
+
+        await chat.compress_large_messages(
+            chat_compress_threshold=4096,
+            message_compress_threshold=2048,
+        )
+        after_tokens = chat.get_tokens()
+        discarded_tokens = before_tokens - after_tokens
+        if discarded_tokens:
+            print(
+                f"{title}: Requesting {after_tokens} tokens; discarded {discarded_tokens} tokens"
+            )
+
+    async def update_presence(self):
+        chat_model_name = self.config["chat_model"]
+        total_tokens = self.config["total_tokens"]
+        total_cost = self.config["total_cost"]
+        await self.bot.update_presence(
+            activity=hikari.Activity(
+                type=hikari.ActivityType.PLAYING,
+                name=f"{total_tokens:,} tokens = {total_cost:,.2f} $",
+                state=chat_model_name,
+            )
+        )
+
+    async def reply(self, message: hikari.Message, answer: str) -> hikari.Message:
+        if len(answer) >= 2000:
+            with tempfile.NamedTemporaryFile(suffix=".log") as f:
+                f.write(answer.encode("utf-8"))
+                answer_file = hikari.File(f.name, filename="message.log")
+                reply = await message.respond(attachment=answer_file, reply=True)
+        else:
+            reply = await message.respond(answer, reply=True)
+
+        self.cached_reply_ids[message.id].add(reply.id)
+        self.cached_messages[message.channel_id][reply.id] = reply
+        return reply
+
+    async def build_chat(self, message: hikari.Message) -> Chat:
+        bot_mention = f"<@{self.bot_id}>"
+
+        messages: list[BaseMessage] = []
+        for message in await self.fetch_all_messages(message, 64):
+            role = "assistant" if message.author.id == self.bot_id else "user"
+            text = cast(str, message.content)
+            text = text.removeprefix(bot_mention).strip()
+
+            if text.lower().startswith("[system]"):
+                if role == "user":
+                    role = "system"
+                    text = text[len("[system]") :].strip()
+                elif role == "assistant":
+                    continue
+                else:
+                    raise ValueError("Unknown role")
+
+            if message.attachments:
+                text += "\n\n" + (await self.fetch_attachment(message))
+                if get_tokens(self.chat_model, text) > 1024 * 3:
+                    raise ValueError("Attachment too large")
+
+            if role == "system":
+                messages.insert(0, SystemMessage(text))
+            elif role == "user":
+                messages.append(HumanMessage(text))
+            elif role == "assistant":
+                messages.append(AIMessage(text))
+
+        return Chat(messages, self.chat_model)
+
+    async def fetch_all_messages(
+        self,
+        message: hikari.Message,
+        limit: int,
+    ) -> List[hikari.Message]:
+        channel = await self.fetch_channel(message.channel_id)
+        if not isinstance(channel, hikari.TextableChannel):
+            raise ValueError("Not a text channel")
+
+        messages = []
+        for i in range(limit):
+            self.cached_messages[channel.id][message.id] = message
+            messages.append(message)
+
+            if message.referenced_message:
+                if message.referenced_message.id in self.cached_messages[channel.id]:
+                    message = self.cached_messages[channel.id][
+                        message.referenced_message.id
+                    ]
+                else:
+                    message = await channel.fetch_message(message.referenced_message.id)
+            else:
+                break
+
+        return messages[::-1]
+
+    @alru_cache(maxsize=64, typed=True, ttl=3600)
+    async def fetch_attachment(self, message: hikari.Message) -> str:
+        if len(message.attachments) > 1:
+            raise ValueError("Too many attachments")
+
+        for attachment in message.attachments:
+            if attachment.size > 1024 * 64:
+                raise ValueError("Attachment too large")
+            elif not attachment.filename.endswith((".txt", ".py")):
+                raise ValueError("Attachment is not text")
+
+            content = await attachment.read()
+            try:
+                return content.decode("utf-8")
+            except UnicodeDecodeError:
+                raise ValueError("Attachment is not text (utf-8)")
+
+
+bot = hikari.GatewayBot(
+    os.environ.get("DISCORD_TOKEN"),
+    intents=hikari.Intents.ALL_MESSAGES | hikari.Intents.MESSAGE_CONTENT,
+)
+
+
+chatgpt = ChatGPT(
+    bot,
+    {"chat_model": os.environ["LANGBOT_CHAT_MODEL"]},
+)
+
+
+@bot.listen()
+async def on_ready(event: hikari.ShardReadyEvent):
+    await chatgpt.on_ready(event)
+
+
+@bot.listen()
+async def on_message(event: hikari.MessageCreateEvent):
+    await chatgpt.on_message(event)
+
+
+@bot.listen()
+async def on_message_edit(event: hikari.MessageUpdateEvent):
+    await chatgpt.on_message_edit(event)
+
+
+@bot.listen()
+async def on_message_delete(event: hikari.MessageDeleteEvent):
+    await chatgpt.on_message_delete(event)
+
+
+def main():
+    bot.run()
+
+
+if __name__ == "__main__":
+    main()
