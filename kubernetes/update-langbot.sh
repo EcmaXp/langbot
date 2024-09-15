d139a06d335764ba7a823e731629627575491233
ecmaxp <ecmaxp@ecmaxp.kr>
2024-09-15 18:49:50 +0900

    feat!: remove gemini

--- kubernetes/update-langbot.sh
+++ kubernetes/update-langbot.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 kubectl --context sigsrv-prod -n langbot apply -f langbot-policy.yaml
 kubectl --context sigsrv-prod -n langbot apply -f langbot-claude.yaml
-kubectl --context sigsrv-prod -n langbot apply -f langbot-gemini.yaml
 kubectl --context sigsrv-prod -n langbot apply -f langbot-gpt-4o.yaml
 ./restart-langbot.sh
