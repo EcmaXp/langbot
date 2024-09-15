80e6b3d6edeaa84eb7d8d6925d95671ca5057663
ecmaxp <ecmaxp@ecmaxp.kr>
2024-09-15 19:10:19 +0900

    feat: change openai model to chatgpt-4o-latest

--- kubernetes/restart-langbot.sh
+++ kubernetes/restart-langbot.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 docker buildx build --platform linux/amd64 -t registry-prod.deer-neon.ts.net/langbot:latest .. --push
 kubectl --context sigsrv-prod -n langbot rollout restart statefulsets langbot-claude
-kubectl --context sigsrv-prod -n langbot rollout restart statefulsets langbot-gpt-4o
+kubectl --context sigsrv-prod -n langbot rollout restart statefulsets langbot-chatgpt-4o
--- kubernetes/update-langbot.sh
+++ kubernetes/update-langbot.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 kubectl --context sigsrv-prod -n langbot apply -f langbot-policy.yaml
 kubectl --context sigsrv-prod -n langbot apply -f langbot-claude.yaml
-kubectl --context sigsrv-prod -n langbot apply -f langbot-gpt-4o.yaml
+kubectl --context sigsrv-prod -n langbot apply -f langbot-chatgpt-4o.yaml
 ./restart-langbot.sh
