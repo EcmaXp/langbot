e0c730496c5db367576bc5d7ff67154e5ebb7ca5
ecmaxp <ecmaxp@ecmaxp.kr>
2024-09-26 00:34:16 +0900

    feat: use gpt-4o model instead of chatgpt-4o-latest

--- kubernetes/update-langbot.sh
+++ kubernetes/update-langbot.sh
@@ -2,5 +2,5 @@
 kubectl --context sigsrv-prod -n langbot apply -f langbot-vault.yaml
 kubectl --context sigsrv-prod -n langbot apply -f langbot-policy.yaml
 kubectl --context sigsrv-prod -n langbot apply -f langbot-claude.yaml
-kubectl --context sigsrv-prod -n langbot apply -f langbot-openai-chatgpt-4o.yaml
+kubectl --context sigsrv-prod -n langbot apply -f langbot-openai-gpt-4o.yaml
 ./restart-langbot.sh
