5927882bdb478a88b0f00a558b8ff127ad7cd940
ecmaxp <ecmaxp@ecmaxp.kr>
2024-09-26 00:34:55 +0900

    feat: add openai-o1 langbot

--- kubernetes/restart-langbot.sh
+++ kubernetes/restart-langbot.sh
@@ -2,3 +2,4 @@
 docker buildx build --platform linux/amd64 -t registry-prod.deer-neon.ts.net/langbot:latest .. --push
 kubectl --context sigsrv-prod -n langbot rollout restart deployment langbot-claude
 kubectl --context sigsrv-prod -n langbot rollout restart deployment langbot-openai-gpt-4o
+kubectl --context sigsrv-prod -n langbot rollout restart deployment langbot-openai-o1
--- kubernetes/update-langbot.sh
+++ kubernetes/update-langbot.sh
@@ -3,4 +3,5 @@ kubectl --context sigsrv-prod -n langbot apply -f langbot-vault.yaml
 kubectl --context sigsrv-prod -n langbot apply -f langbot-policy.yaml
 kubectl --context sigsrv-prod -n langbot apply -f langbot-claude.yaml
 kubectl --context sigsrv-prod -n langbot apply -f langbot-openai-gpt-4o.yaml
+kubectl --context sigsrv-prod -n langbot apply -f langbot-openai-o1.yaml
 ./restart-langbot.sh
