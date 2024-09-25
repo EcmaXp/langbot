6b223e0d971e229da6897e4c40d505da698513ce
ecmaxp <ecmaxp@ecmaxp.kr>
2024-09-26 00:30:20 +0900

    refactor: refactoring kubernetes resources
    - use shared ServiceAccount/VaultAuth/VaultDynamicSecret/Secret
    - use Deployment instead of StatefulSet

--- kubernetes/update-langbot.sh
+++ kubernetes/update-langbot.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
+kubectl --context sigsrv-prod -n langbot apply -f langbot-vault.yaml
 kubectl --context sigsrv-prod -n langbot apply -f langbot-policy.yaml
 kubectl --context sigsrv-prod -n langbot apply -f langbot-claude.yaml
-kubectl --context sigsrv-prod -n langbot apply -f langbot-chatgpt-4o.yaml
+kubectl --context sigsrv-prod -n langbot apply -f langbot-openai-chatgpt-4o.yaml
 ./restart-langbot.sh
