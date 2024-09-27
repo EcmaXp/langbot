84de96ea148ef90d56153b652bd51721bd9413bc
ecmaxp <ecmaxp@ecmaxp.kr>
2024-09-28 02:17:24 +0900

    feat: add o1-mini langbot

--- kubernetes/restart-langbot.sh
+++ kubernetes/restart-langbot.sh
@@ -3,3 +3,4 @@ docker buildx build --platform linux/amd64 -t registry-prod.deer-neon.ts.net/lan
 kubectl --context sigsrv-prod -n langbot rollout restart deployment langbot-claude
 kubectl --context sigsrv-prod -n langbot rollout restart deployment langbot-openai-gpt-4o
 kubectl --context sigsrv-prod -n langbot rollout restart deployment langbot-openai-o1
+kubectl --context sigsrv-prod -n langbot rollout restart deployment langbot-openai-o1-mini
