7aa2d339b490768de7c232b5890ff26d1fb90e2d
ecmaxp <ecmaxp@ecmaxp.kr>
2025-05-06 03:40:24 +0900

    feat: change openai models

--- kubernetes/restart-langbot.sh
+++ kubernetes/restart-langbot.sh
@@ -2,5 +2,5 @@
 docker buildx build --platform linux/amd64 -t registry-t1.deer-neon.ts.net/langbot:latest .. --push
 kubectl --context sigsrv-prod -n langbot rollout restart deployment langbot-claude
 kubectl --context sigsrv-prod -n langbot rollout restart deployment langbot-openai-gpt-4o
-kubectl --context sigsrv-prod -n langbot rollout restart deployment langbot-openai-o1
-kubectl --context sigsrv-prod -n langbot rollout restart deployment langbot-openai-o3-mini
+kubectl --context sigsrv-prod -n langbot rollout restart deployment langbot-openai-o3
+kubectl --context sigsrv-prod -n langbot rollout restart deployment langbot-openai-o4-mini
--- kubernetes/update-langbot.sh
+++ kubernetes/update-langbot.sh
@@ -3,6 +3,6 @@ kubectl --context sigsrv-prod -n langbot apply -f langbot-vault.yaml
 kubectl --context sigsrv-prod -n langbot apply -f langbot-policy.yaml
 kubectl --context sigsrv-prod -n langbot apply -f langbot-claude.yaml
 kubectl --context sigsrv-prod -n langbot apply -f langbot-openai-gpt-4o.yaml
-kubectl --context sigsrv-prod -n langbot apply -f langbot-openai-o1.yaml
-kubectl --context sigsrv-prod -n langbot apply -f langbot-openai-o3-mini.yaml
+kubectl --context sigsrv-prod -n langbot apply -f langbot-openai-o3.yaml
+kubectl --context sigsrv-prod -n langbot apply -f langbot-openai-o4-mini.yaml
 ./restart-langbot.sh
